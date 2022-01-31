575c2c78c833837cafaccb90c39f40754ce3f21a
Siyateagan <siyateagan@gmail.com>
2022-01-31 19:37:27 +0300

    Add Configuration class.

---
+++ epam-gmail-task/WebDriver/Configuration.cs
@@ -0,0 +1,18 @@
+﻿using System.Configuration;
+
+
+namespace epam_gmail_task.WebDriver
+{
+    public class Configuration
+    {
+        public static string GetEnviromentVar(string var, string defautlValue) =>
+            ConfigurationManager.AppSettings[var] ?? defautlValue;
+
+
+        public static string ElementTimeout => GetEnviromentVar("ElementTimeout", "30");
+
+        public static string Browser => GetEnviromentVar("Browser", "Chrome");
+
+        public static string StartUrl => GetEnviromentVar("StartUrl", "https://www.google.com/intl/ru/gmail/about/");
+    }
+}
