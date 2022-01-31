ba0254692f72986ede95de239a7768373884d244
Siyateagan <siyateagan@gmail.com>
2022-01-31 19:40:29 +0300

    Add BasePage.

---
+++ epam-gmail-task/PageObjects/BasePage.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using System.Linq;
+
+namespace epam_gmail_task.WebDriver
+{
+    public abstract class BasePage
+    {
+        private static IWebDriver _driver;
+
+        protected BasePage(IWebDriver driver) => _driver = driver;
+
+        public IWebDriver GetDriver() => _driver;
+
+        public bool IsElementPresent(By locator) => _driver.FindElements(locator).Count() > 0;
+    }
+}
