7f659f668078fb83269ea61f868a60306abac2bb
Siyateagan <siyateagan@gmail.com>
2022-02-01 18:17:07 +0300

    Add Browser class.

---
+++ epam-gmail-task/WebDriver/Browser.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace epam_gmail_task.WebDriver
+{
+    public class Browser
+    {
+        private static Browser _currentInstance = null!;
+        private static IWebDriver _driver = null!;
+        public static BrowserFactory.BrowserType _currentBrowser;
+        public static int ImplWait;
+        public static double _timeoutForElement;
+        private static string _browserName = string.Empty;
+
+        private Browser()
+        {
+            InitParams();
+            _driver = BrowserFactory.GetDriver(_currentBrowser, ImplWait);
+        }
+
+        private static void InitParams()
+        {
+            ImplWait = Convert.ToInt32(Configuration.ElementTimeout);
+            _timeoutForElement = Convert.ToDouble(Configuration.ElementTimeout);
+            _browserName = Configuration.Browser;
+        }
+
+        public static Browser Instance => _currentInstance ??= new Browser();
+
+        public static void WindowMaximise() => _driver.Manage().Window.Maximize();
+
+        public static void NavigateTo(string url) => _driver.Navigate().GoToUrl(url);
+
+        public static IWebDriver GetDriver() => _driver;
+
+        public static void Quit() => _driver.Quit();
+    }
+}
