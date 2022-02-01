b441efff24d39b11d00614fb5b5ab262b2ff65f6
Siyateagan <siyateagan@gmail.com>
2022-02-01 18:57:53 +0300

    Add AboutPage.

---
+++ epam-gmail-task/PageObjects/AboutPage.cs
@@ -0,0 +1,20 @@
+﻿using epam_gmail_task.WebDriver;
+using OpenQA.Selenium;
+
+namespace epam_gmail_task.PageObjects
+{
+    public class AboutPage : BasePage
+    {
+        private static readonly By AboutLabel = 
+            By.XPath("//div[@class='feature__chapter__title feature__chapter__title--long']");
+
+        private readonly BaseElement _signInLink = new BaseElement(By.XPath("//a[text()='Войти']"));
+        public AboutPage() : base(AboutLabel) { }
+
+        public SignInPage GoToSignInPage()
+        {
+            _signInLink.Click();
+            return new SignInPage();
+        }
+    }
+}
