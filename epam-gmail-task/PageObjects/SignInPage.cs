31cd9502e319ef8a974fe4a2877a50785501acbe
Siyateagan <siyateagan@gmail.com>
2022-02-01 18:57:02 +0300

    Add SignInPage.

---
+++ epam-gmail-task/PageObjects/SignInPage.cs
@@ -0,0 +1,13 @@
+﻿using epam_gmail_task.WebDriver;
+using OpenQA.Selenium;
+
+namespace epam_gmail_task.PageObjects
+{
+    public class SignInPage : BasePage
+    {
+        private static readonly By SignInLabel = 
+            By.XPath("//span[text()='Перейти в Gmail']");
+
+        public SignInPage() : base(SignInLabel) { }
+    }
+}
