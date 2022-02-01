1da7b625b5b8188398231f03e6daa56b9a04b844
Siyateagan <siyateagan@gmail.com>
2022-02-01 18:20:17 +0300

    Add BaseTest class.

---
+++ epam-gmail-task/Tests/BaseTest.cs
@@ -0,0 +1,22 @@
+﻿using epam_gmail_task.WebDriver;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace epam_gmail_task.Tests
+{
+    public class BaseTest
+    {
+        [TestInitialize]
+        public virtual void InitTest()
+        {
+            Browser Browser = Browser.Instance;
+            Browser.WindowMaximise();
+            Browser.NavigateTo(Configuration.StartUrl);
+        }
+
+        [TestCleanup]
+        public virtual void CleanTest()
+        {
+            Browser.Quit();
+        }
+    }
+}
