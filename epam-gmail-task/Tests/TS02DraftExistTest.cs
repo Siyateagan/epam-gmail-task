37d5de6517fbade72bce4567894fcb5b6290a558
Siyateagan <siyateagan@gmail.com>
2022-02-02 18:49:02 +0300

    Implement DraftExistTest.

---
+++ epam-gmail-task/Tests/TS02DraftExistTest.cs
@@ -0,0 +1,29 @@
+﻿using epam_gmail_task.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace epam_gmail_task.Tests
+{
+    [TestClass]
+    public class TS02DraftExistTest : BaseTest
+    {
+        [TestMethod]
+        public void TC04_Remove_Draft_NotExist()
+        {
+            AboutPage aboutPage = new AboutPage();
+            aboutPage.GoToSignInPage();
+            SignIn();
+
+            MainPage mainPage = new MainPage();
+            mainPage.ClickDraftLink();
+            string subject = mainPage.GetMessageSubject();
+            Assert.AreEqual("Test Subject", subject);
+
+            mainPage.SelectMessage();
+            mainPage.DeleteSelectedDrafts();
+            Assert.IsTrue(mainPage.GetNoSavedDraftMessage().Contains("Нет сохраненных черновиков."));
+
+            mainPage.ManageAccountClick();
+            mainPage.SignOutClick();
+        }
+    }
+}
