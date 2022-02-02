b58d0cca7354c65f15682a46ae4436a7e9e89dae
Siyateagan <siyateagan@gmail.com>
2022-02-02 15:25:58 +0300

    Add SaveDraftTest.

---
+++ epam-gmail-task/Tests/SaveDraftTest.cs
@@ -0,0 +1,42 @@
+﻿using epam_gmail_task.PageObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+
+namespace epam_gmail_task.Tests
+{
+    [TestClass]
+    public class SaveDraftTest : BaseTest
+    {
+        [TestMethod]
+        public void TC01_Navigate_ToSignInPage_PageExist()
+        {
+            AboutPage aboutPage = new AboutPage();
+            aboutPage.GoToSignInPage();
+        }
+
+        [TestMethod]
+        public void TC02_Check_CurrentAccount_Matches()
+        {
+            SignIn();
+            MainPage mainPage = new MainPage();
+            mainPage.ManageAccountClick();
+            Assert.AreEqual(mainPage.GetCurrentAccountMail(), "gt016618@gmail.com");
+        }
+
+        [TestMethod]
+        public void TC03_Check_MailMessage_Saved()
+        {
+            MainPage mainPage = new MainPage();
+            mainPage.ClickWrite();
+            // TODO: Add to data.
+            mainPage.EnterMessageData("siyateagan@gmail.com", "Test Subject", "Test Message");
+            mainPage.WaitForDraftSave();
+            Assert.AreEqual(mainPage.GetDraftStatus(), "Черновик сохранен");
+
+            mainPage.ManageAccountClick();
+            mainPage.SignOutClick();
+
+        }
+    }
+}
