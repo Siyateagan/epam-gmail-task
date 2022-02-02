63625fa43be19fd0f1e151d994db83554cf593d1
Siyateagan <siyateagan@gmail.com>
2022-02-02 15:30:24 +0300

    Add headless argument.

--- epam-gmail-task/WebDriver/BrowserFactory.cs
+++ epam-gmail-task/WebDriver/BrowserFactory.cs
@@ -23,6 +23,7 @@ namespace epam_gmail_task.WebDriver
                         var service = ChromeDriverService.CreateDefaultService();
                         var option = new ChromeOptions();
                         option.AddArgument("disable-infobars");
+                        option.AddArgument("--headless");
                         option.AddExcludedArgument("enable-automation");
 
                         driver = new ChromeDriver(service, option, TimeSpan.FromSeconds(timeOutSec));
