afca569249ae55e287e3a4b6bb5606dd0c56bcd8
Siyateagan <siyateagan@gmail.com>
2022-02-04 23:14:02 +0300

    Add BaseElement element inheritance from IWebElement.

--- epam-gmail-task/WebObjects/BaseElement.cs
+++ epam-gmail-task/WebObjects/BaseElement.cs
@@ -2,10 +2,12 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using System;
+using System.Collections.ObjectModel;
+using System.Drawing;
 
 namespace epam_gmail_task.PageObjects
 {
-    public class BaseElement
+    public class BaseElement : IWebElement
     {
         protected By _locator;
         private IWebDriver _driver = Browser.GetDriver();
@@ -50,5 +52,63 @@ namespace epam_gmail_task.PageObjects
             WaitForIsVisible();
             return Browser.GetDriver().FindElement(_locator).Text;
         }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Submit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDomAttribute(string attributeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetProperty(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetDomProperty(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetCssValue(string propertyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ISearchContext GetShadowRoot()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IWebElement FindElement(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ReadOnlyCollection<IWebElement> FindElements(By by)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string? TagName { get; }
+        public string? Text { get; }
+        public bool Enabled { get; }
+        public bool Selected { get; }
+        public Point Location { get; }
+        public Size Size { get; }
+        public bool Displayed { get; }
     }
 }
