7f53684a45bc98fc8a782450cdd65c2d11376976
Siyateagan <siyateagan@gmail.com>
2022-02-04 22:36:46 +0300

    Add the functionality of getting login and password from the config.

--- epam-gmail-task/WebDriver/Configuration.cs
+++ epam-gmail-task/WebDriver/Configuration.cs
@@ -1,4 +1,5 @@
-﻿using System.Configuration;
+﻿using System.Collections.Specialized;
+using System.Configuration;
 
 
 namespace epam_gmail_task.WebDriver
@@ -8,10 +9,19 @@ namespace epam_gmail_task.WebDriver
         public static string GetEnviromentVar(string var, string defautlValue) =>
             ConfigurationManager.AppSettings[var] ?? defautlValue;
 
+        public static string GetEnviromentVarBySection(string sectionName, string var, string defautlValue)
+        {
+            var section = ConfigurationManager.GetSection(sectionName) as NameValueCollection;
+            return section![var] ?? defautlValue;
+        }
+           
+
         public static string ElementTimeout => GetEnviromentVar("ElementTimeout", "30");
 
         public static string Browser => GetEnviromentVar("Browser", "Chrome");
 
         public static string StartUrl => GetEnviromentVar("StartUrl", "https://www.google.com/intl/ru/gmail/about/");
+        public static string Login => GetEnviromentVarBySection("users", "Login", "gt016618");
+        public static string Password => GetEnviromentVarBySection("users", "Password", "mAPM6SWd");
     }
 }
