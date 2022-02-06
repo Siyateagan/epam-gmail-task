63f8fd88ee78559ad501c61a7fe49187749dc2ac
Siyateagan <siyateagan@gmail.com>
2022-02-06 19:43:34 +0300

    Add work with frames. Since now the account settings elements are wrapped in frames.

--- epam-gmail-task/WebObjects/MainPageBase.cs
+++ epam-gmail-task/WebObjects/MainPageBase.cs
@@ -1,10 +1,15 @@
 ﻿using epam_gmail_task.WebDriver;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 namespace epam_gmail_task.PageObjects
 {
     public abstract class MainPageBase : BasePage
     {
+        private WebDriverWait _frameWait = 
+            new WebDriverWait(Browser.GetDriver(), Browser.TimeoutForElement);
+
         protected MainPageBase(By titleLocator) : base(titleLocator) { }
 
         protected readonly BaseElement _writeDiv =
@@ -13,23 +18,36 @@ namespace epam_gmail_task.PageObjects
         protected readonly BaseElement _manageAccountLink =
             new BaseElement(By.XPath("(//a[@role='button'])[last()]"));
 
-        protected readonly BaseElement _signOutLink =
-            new BaseElement(By.XPath("//a[text()='Выйти']"));
+        protected readonly BaseElement _signOutDiv =
+            new BaseElement(By.XPath("//div[text()='Выйти']"));
 
         protected readonly BaseElement _selectMessageDiv =
             new BaseElement(By.XPath("//div[@aria-checked='false']"));
 
         protected readonly BaseElement _accountMailDiv =
-            new BaseElement(By.XPath("//div[@class='gb_mb']"));
+            new BaseElement(By.XPath("//div[text()='gt016618@gmail.com']"));
 
         protected readonly BaseElement _draftLink =
             new BaseElement(By.XPath("//a[text()='Черновики']"));
 
+        protected readonly BaseElement _accountFrame =
+            new BaseElement(By.XPath("//iframe[@role='presentation' and @style='height: 100%; width: 100%;']"));
+
         public void ClickWrite() => _writeDiv.Click();
         public void ManageAccountClick() => _manageAccountLink.Click();
-        public void SignOutClick() => _signOutLink.Click();
         public void SelectMessage() => _selectMessageDiv.Click();
         public void ClickDraftLink() => _draftLink.Click();
-        public string GetCurrentAccountMail() => _accountMailDiv.GetText();
+        public string GetCurrentAccountMail()
+        {
+            _frameWait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(_accountFrame._locator));
+            var accoutMail = _accountMailDiv.GetText();
+            Browser.GetDriver().SwitchTo().DefaultContent();
+            return accoutMail;
+        }
+        public void SignOutClick()
+        {
+            _frameWait.Until(ExpectedConditions.FrameToBeAvailableAndSwitchToIt(_accountFrame._locator));
+            _signOutDiv.Click();
+        }
     }
 }
