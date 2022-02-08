8bfea5bbde88d5f99ed0f1d51442420753b7e143
Siyateagan <siyateagan@gmail.com>
2022-02-08 21:41:35 +0300

    Add RemoteChrome browser type.

--- epam-gmail-task/WebDriver/DriverFactory.cs
+++ epam-gmail-task/WebDriver/DriverFactory.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Remote;
 using System;
 
 namespace epam_gmail_task.WebDriver
@@ -10,6 +11,7 @@ namespace epam_gmail_task.WebDriver
         public enum BrowserType
         {
             Chrome,
+            RemoteChrome,
             Firefox
         }
 
@@ -22,11 +24,11 @@ namespace epam_gmail_task.WebDriver
                 case BrowserType.Chrome:
                     {
                         var service = ChromeDriverService.CreateDefaultService();
-                        var option = new ChromeOptions();
-                        option.AddArgument("disable-infobars");
-                        option.AddExcludedArgument("enable-automation");
+                        var options = new ChromeOptions();
+                        options.AddArgument("disable-infobars");
+                        options.AddExcludedArgument("enable-automation");
 
-                        driver = new ChromeDriver(service, option, TimeSpan.FromSeconds(timeOutSec));
+                        driver = new ChromeDriver(service, options, TimeSpan.FromSeconds(timeOutSec));
                         break;
                     }
                 case BrowserType.Firefox:
@@ -35,6 +37,14 @@ namespace epam_gmail_task.WebDriver
                         var option = new FirefoxOptions();
 
                         driver = new FirefoxDriver(service, option, TimeSpan.FromSeconds(timeOutSec));
+                        break;
+                    }
+                case BrowserType.RemoteChrome:
+                    {
+                        var options = new ChromeOptions();
+                        options.AddArgument("disable-infobars");
+                        options.AddExcludedArgument("enable-automation");
+                        driver = new RemoteWebDriver(new Uri(Configuration.RemoteDriverUri), options);
                         break;
                     }
             }
