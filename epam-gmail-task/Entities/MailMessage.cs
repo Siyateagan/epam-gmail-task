aef2f4d4bdbd126762bb960c990341e59e3a928e
Siyateagan <siyateagan@gmail.com>
2022-02-09 23:53:59 +0300

    Add mail message entity.

---
+++ epam-gmail-task/Entities/MailMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace epam_gmail_task.Entities
+{
+    public class MailMessage
+    {
+        public readonly string receiver;
+        public readonly string subject;
+        public readonly string message;
+
+        public MailMessage(string receiver, string subject, string message)
+        {
+            this.receiver = receiver;
+            this.subject = subject;
+            this.message = message;
+        }
+    }
+}
