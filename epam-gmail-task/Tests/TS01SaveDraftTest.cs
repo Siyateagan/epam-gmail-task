dd5d553ccf4b6c76779c8584a16ae539bb4f108e
Siyateagan <siyateagan@gmail.com>
2022-02-09 22:07:35 +0300

    Get User data  using DataSource.

--- epam-gmail-task/Tests/TS01SaveDraftTest.cs
+++ epam-gmail-task/Tests/TS01SaveDraftTest.cs
@@ -1,18 +1,25 @@
-﻿using epam_gmail_task.PageObjects;
-using epam_gmail_task.WebDriver;
+﻿using epam_gmail_task.Entities;
+using epam_gmail_task.PageObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace epam_gmail_task.Tests
 {
+    [DeploymentItem(@"Resourses")]
     [TestClass]
     public class TS01SaveDraftTest : BaseTest
     {
+        //TODO: Check multiple users
+        [DeploymentItem(@"Resourses")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\UserData.csv", "UserData#csv", DataAccessMethod.Sequential)]
         [TestMethod]
         public void TC01_Check_CurrentAccount_Matches()
         {
+            User currentUser = GetUser();
             MainPage mainPage = new MainPage();
             mainPage.ManageAccountClick();
-            string expectedMail = Configuration.Login + "@gmail.com";
+
+            string expectedMail = currentUser.email + "@gmail.com";
             Assert.AreEqual(mainPage.GetCurrentAccountMail(), expectedMail);
         }
 
--- epam-gmail-task/WebDriver/Configuration.cs
+++ epam-gmail-task/WebDriver/Configuration.cs
@@ -19,8 +19,6 @@ namespace epam_gmail_task.WebDriver
         public static string ElementTimeout => GetEnviromentVar("ElementTimeout", "30");
         public static string Browser => GetEnviromentVar("Browser", "Chrome");
         public static string StartUrl => GetEnviromentVar("StartUrl", "https://www.google.com/intl/ru/gmail/about/");
-        public static string Login => GetEnviromentVarBySection("users", "Login", "gt016618");
-        public static string Password => GetEnviromentVarBySection("users", "Password", "mAPM6SWd");
         public static string RemoteDriverUri => GetEnviromentVarBySection("RemoteDriverUri", "GridUri", "http://localhost:4444/wd/hub");
     }
 }
