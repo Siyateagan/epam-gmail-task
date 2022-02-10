a37c6c7fbc60220c36aaf6d47258daae5b6a0ed8
Siyateagan <siyateagan@gmail.com>
2022-02-10 03:02:45 +0300

    Refactor TS02DraftExistTest methods as Data-Driven.

--- epam-gmail-task/Tests/TS02DraftExistTest.cs
+++ epam-gmail-task/Tests/TS02DraftExistTest.cs
@@ -1,4 +1,5 @@
-﻿using epam_gmail_task.PageObjects;
+﻿using epam_gmail_task.Entities;
+using epam_gmail_task.PageObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace epam_gmail_task.Tests
@@ -6,21 +7,37 @@ namespace epam_gmail_task.Tests
     [TestClass]
     public class TS02DraftExistTest : BaseTest
     {
+        [DeploymentItem(@"Resourses")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\UserData.csv", "UserData#csv", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public override void TC00_TestPreparation() //TODO preparement
+        {
+            base.TC00_TestPreparation();
+            MainPage mainPage = new MainPage();
+            mainPage.ClickDraftLink();
+        }
+
+        [DeploymentItem(@"Resourses")]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV",
+            "|DataDirectory|\\MailMessageData.csv", "MailMessageData#csv", DataAccessMethod.Sequential)]
         [TestMethod]
         public void TC03_Remove_Draft_NotExist()
         {
-            MainPage mainPage = new MainPage();
-            mainPage.ClickDraftLink();
             DraftPage draftPage = new DraftPage();
+            draftPage.ClickDraftLink();
 
-            string subject = draftPage.GetMessageSubject();
-            Assert.AreEqual("Test Subject", subject);
+            // TODO Move to another layer?
+            string receiver = TestContext.DataRow["receiver"].ToString();
+            string subject = TestContext.DataRow["subject"].ToString();
+            string message = TestContext.DataRow["message"].ToString();
+            MailMessage mailMessage = new MailMessage(receiver, subject, message);
+            Assert.AreEqual(mailMessage.subject, draftPage.GetSubjectValueByText(mailMessage.subject));
 
-            draftPage.SelectMessage();
+            draftPage.SelectLastMessage();
             draftPage.DeleteSelectedDrafts();
-            Assert.IsTrue(draftPage.GetNoSavedDraftMessage().Contains("Нет сохраненных черновиков."));
 
-            SignOut(mainPage);
+            Assert.IsFalse(draftPage.CheckSubjectExistsByText(mailMessage.subject));
         }
     }
 }
