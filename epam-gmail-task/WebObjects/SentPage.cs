95dd727409d54628b0f3d9733d35cdfd34e571e3
Siyateagan <siyateagan@gmail.com>
2022-02-15 18:20:13 +0300

    Add Pages.

---
+++ epam-gmail-task/WebObjects/SentPage.cs
@@ -0,0 +1,15 @@
+﻿using OpenQA.Selenium;
+
+namespace epam_gmail_task.PageObjects
+{
+    public class SentPage : MainPageBase
+    {
+        private readonly string pageInput = "in:sent ";
+        private static readonly By SentLabel = By.XPath("//input[@aria-label='Поиск в почте']");//Remove
+
+        public SentPage() : base(SentLabel)
+        {
+            WaitPageIsOpen(pageInput);
+        } 
+    }
+}
---
+++ epam-gmail-task/WebObjects/SubjectPage.cs
@@ -0,0 +1,16 @@
+﻿using epam_gmail_task.PageObjects;
+using epam_gmail_task.WebDriver;
+using OpenQA.Selenium;
+
+namespace epam_gmail_task.WebObjects
+{
+    public class SubjectPage : BasePage
+    {
+        private static readonly By SubjectLabel = By.Id("blog-ideas-output");
+        private readonly BaseElement _subjectLabel = new BaseElement(By.XPath("//div[@id='blog-ideas-output']"));
+
+        public SubjectPage() : base(SubjectLabel) { }
+
+        public string GetSubject() => _subjectLabel.GetText();
+    }
+}
