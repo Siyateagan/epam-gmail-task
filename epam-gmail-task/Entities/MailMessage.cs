ecbe9d69e9054eeb9c59127cb2e5cae393c4f5c2
Siyateagan <siyateagan@gmail.com>
2022-02-15 18:19:13 +0300

    Add MainMessage constructor.

--- epam-gmail-task/Entities/MailMessage.cs
+++ epam-gmail-task/Entities/MailMessage.cs
@@ -8,6 +8,13 @@ namespace epam_gmail_task.Entities
         public readonly string subject;
         public readonly string message;
 
+        public MailMessage(string receiver = "Receiver", string subject = "Subject", string message = "Message")
+        {
+            this.receiver = receiver;
+            this.subject = subject;
+            this.message = message;
+        }
+
         public MailMessage(TestContext testContext)
         {
             receiver = testContext.DataRow["receiver"].ToString();
