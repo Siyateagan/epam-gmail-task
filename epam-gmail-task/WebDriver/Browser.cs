6a08663e27bdfa95e11492a1c433be1fd0166a52
Siyateagan <siyateagan@gmail.com>
2022-02-15 18:22:46 +0300

    Implement methods to get ,essage subject.

--- epam-gmail-task/WebDriver/Browser.cs
+++ epam-gmail-task/WebDriver/Browser.cs
@@ -1,4 +1,5 @@
-﻿using OpenQA.Selenium;
+﻿using epam_gmail_task.WebObjects;
+using OpenQA.Selenium;
 using System;
 
 namespace epam_gmail_task.WebDriver
@@ -16,10 +17,8 @@ namespace epam_gmail_task.WebDriver
             InitParams();
             Enum.TryParse(Configuration.Browser, out _currentBrowser);
         }
-
         public static void StartDriver() =>
             _driver = DriverFactory.InitializeDriver(_currentBrowser, ImplWait);
-
         private static void InitParams()
         {
             ImplWait = Convert.ToInt32(Configuration.ElementTimeout);
@@ -27,13 +26,25 @@ namespace epam_gmail_task.WebDriver
         }
 
         public static Browser Instance => _currentInstance ??= new Browser();
-
         public static void WindowMaximise() => _driver.Manage().Window.Maximize();
-
         public static void NavigateTo(string url) => _driver.Navigate().GoToUrl(url);
-
         public static IWebDriver GetDriver() => _driver;
-
         public static void Quit() => _driver.Quit();
+        public static void OpenNewTab(string url)
+        {
+            ((IJavaScriptExecutor)_driver).ExecuteScript("window.open();");
+            var newHandles = _driver.WindowHandles;
+            _driver.SwitchTo().Window(newHandles[1]);
+            _driver.Navigate().GoToUrl(url);
+        }
+        public static string GetRandomSubject()
+        {
+            string currentHandle = _driver.CurrentWindowHandle;
+            OpenNewTab("https://capitalizemytitle.com/random-topic-generator/");
+            SubjectPage subjectPage = new SubjectPage();
+            string subject = subjectPage.GetSubject();
+            _driver.SwitchTo().Window(currentHandle);
+            return subject;
+        }
     }
 }
