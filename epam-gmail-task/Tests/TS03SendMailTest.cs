52f0e849e2a6eb4c99fb35791e6d054dbbb6587f
Siyateagan <siyateagan@gmail.com>
2022-02-15 18:24:02 +0300

    Implement Test.

---
+++ epam-gmail-task/Tests/TS03SendMailTest.cs
@@ -0,0 +1,37 @@
+﻿using epam_gmail_task.Entities;
+using epam_gmail_task.PageObjects;
+using epam_gmail_task.WebDriver;
+using epam_gmail_task.WebObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting.Logging;
+using System;
+using System.Threading;
+
+namespace epam_gmail_task.Tests
+{
+    [TestClass]
+    public class TS03SendMailTest : BaseTest
+    {
+        [TestMethod]
+        public void TC05_Check_Message_Sent()
+        {
+            SignIn();
+            MainPage mainPage = new MainPage();
+            mainPage.ClickWrite();
+
+            string receiver = Configuration.Login + "@gmail.com";
+            string subject = Browser.GetRandomSubject();
+
+            MailMessage mailMessage = new MailMessage(receiver: receiver, subject: subject);
+            mainPage.EnterMessageData(mailMessage);
+            mainPage.ClickSendMessage();
+
+            mainPage.NavigateToSentMessages();
+            SentPage sentPage = new SentPage();
+            Assert.IsTrue(sentPage.CheckSubjectExistsByText(mailMessage.subject));
+
+            sentPage.ClickIncomingMessages();
+            Assert.IsTrue(mainPage.CheckSubjectExistsByText(mailMessage.subject));
+        }
+    }
+}
